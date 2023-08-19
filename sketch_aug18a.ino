e3c64ac990b7162f3b2a0d5996a602eae237228c
SantiagoPo <105111658+SantiagoPo@users.noreply.github.com>
2023-08-18 20:20:34 -0500

    Trabajo Terminado

---
+++ sketch_aug18a.ino
@@ -0,0 +1,18 @@
+int LED = 13;
+int buttonPin = 11;
+bool val;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  val = digitalRead(buttonPin);
+  if (val) {  
+    digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
+  }
+}
